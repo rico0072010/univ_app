eb65e1d2df0d2a2cb1d2af5aefb84aa40a5510fd
Danil Dolin <danildolin96@gmail.com>
2020-09-21 16:29:38 +0500

    Add restrictions to controllers

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,7 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: %i[show edit update]
+  skip_before_action :require_user, only: %i[new create]
+  before_action :require_same_student, only: %i[edit update]
 
   def index
     @students = Student.all
@@ -35,10 +37,17 @@ class StudentsController < ApplicationController
   private
 
   def student_params
-    params.require(:student).permit(:name, :email)
+    params.require(:student).permit(:name, :email, :password, :password_confirmation)
   end
 
   def set_student
     @student = Student.find(params[:id])
+  end
+
+  def require_same_student
+    if current_user != @student
+      flash[:notice] = 'You can only edit your own profile'
+      redirect_to student_path(current_user)
+    end
   end
 end
